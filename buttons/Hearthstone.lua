90b221d8921ccd782374206ff3794853b9637d25
Adrian L Lange <mail@p3lim.net>
2020-09-16 22:12:55 +0200

    Fix hearthstone button and add more toys

--- buttons/Hearthstone.lua
+++ buttons/Hearthstone.lua
@@ -8,29 +8,35 @@ local hearthstoneToys = {
 	64488, -- The Innkeeper's Daughter
 	163045, -- Headless Horeseman's Hearthstone
 	162973, -- Greatfather Winter's Hearthstone
+	165669, -- Lunar Elder's Hearthstone
+	166747, -- Brewfest Reveler's Hearthstone
+	166746, -- Fire Eater's Hearthstone
+	168907, -- Holographic Digitalization Hearthstone
+	165802, -- Noble Gardener's Hearthstone
+	165670, -- Peddlefeet's Lovely Hearthstone
+	172179, -- Eternal Traveler's Hearthstone
 }
 
 local Button = CreateFrame('Button', C.Name .. 'HearthstoneButton', nil, 'SecureActionButtonTemplate')
-
-function E:TOYS_UPDATED()
+Button:SetAttribute('type', 'macro') -- item doesn't like modifications in PreClick
+Button:SetScript('PreClick', function()
 	if(InCombatLockdown()) then
 		return
 	end
 
-	local toys = ''
+	table.wipe(toys)
 	for _, itemID in next, hearthstoneToys do
 		if(PlayerHasToy(itemID)) then
-			toys = toys .. 'item:' .. itemID .. ', '
+			table.insert(toys, itemID)
 		end
 	end
 
 	if(#toys > 0) then
 		-- Pick a random toy
-		Button:SetAttribute('type', 'macro')
-		Button:SetAttribute('macrotext', '/castrandom ' .. strtrim(toys, ', '))
+		Button:SetAttribute('macrotext', '/cast item:' .. toys[math.random(#toys)])
+		-- /castrandom is broken, has been for years
 	else
 		-- Hearthstone
-		Button:SetAttribute('type', 'item')
-		Button:SetAttribute('item', 6948)
+		Button:SetAttribute('macrotext', '/cast item:' .. 6948)
 	end
-end
+end)
